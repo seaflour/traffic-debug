68c95b0899422724692c83ef3d7693b1769e1b04
Matthew Chin <Matthew Chin>
2016-10-23 23:48:28 +0100

    More merge conflict fixes and whitespace formatting

--- callback_detect_stream.h
+++ callback_detect_stream.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef CALLBACK_DETECT_STREAM_H
 #define CALLBACK_DETECT_STREAM_H
 
@@ -13,21 +12,22 @@
 #include "detect_stream.h"
 
 /* help from http://www.tcpdump.org/pcap.html */
+
 /* IP header */
 struct sniff_ip {
-	u_char ip_vhl;			/* version << 4 | header length >> 2 */
-	u_char ip_tos;			/* type of service */
-	u_short ip_len;			/* total length */
-	u_short ip_id;			/* identification */
-	u_short ip_off;			/* fragment offset field */
+    u_char ip_vhl; /* version << 4 | header length >> 2 */
+    u_char ip_tos; /* type of service */
+    u_short ip_len; /* total length */
+    u_short ip_id; /* identification */
+    u_short ip_off; /* fragment offset field */
 #define IP_RF 0x8000		/* reserved fragment flag */
 #define IP_DF 0x4000		/* dont fragment flags */
 #define IP_MF 0x2000		/* more fragments flag */
 #define IP_OFFMASK 0x1fff	/* mask for fragmented bits */
-	u_char ip_ttl;			/* time to live */
-	u_char ip_p;			/* protocol */
-	u_short ip_sum;			/* checksum */
-	struct in_addr ip_src, ip_dst; 	/* source and dest addresses */
+    u_char ip_ttl; /* time to live */
+    u_char ip_p; /* protocol */
+    u_short ip_sum; /* checksum */
+    struct in_addr ip_src, ip_dst; /* source and dest addresses */
 };
 
 #define SIZE_ETHERNET 14
@@ -36,49 +36,6 @@ struct sniff_ip {
 extern pcap_t *handle;
 extern char *streamip;
 
-void callback_detect_stream(u_char *arg, const struct pcap_pkthdr *pkthdr, const u_char *packet); 
+void callback_detect_stream(u_char *arg, const struct pcap_pkthdr *pkthdr, const u_char *packet);
 
 #endif
-=======
-#ifndef CALLBACK_DETECT_STREAM_H
-#define CALLBACK_DETECT_STREAM_H
-
-#include <pcap.h>
-#include <sys/types.h>
-#include <netinet/ip.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include "detect_stream.h"
-#include "global.h"
-
-/* help from http://www.tcpdump.org/pcap.html */
-/* IP header */
-struct sniff_ip {
-	u_char ip_vhl;			/* version << 4 | header length >> 2 */
-	u_char ip_tos;			/* type of service */
-	u_short ip_len;			/* total length */
-	u_short ip_id;			/* identification */
-	u_short ip_off;			/* fragment offset field */
-#define IP_RF 0x8000		/* reserved fragment flag */
-#define IP_DF 0x4000		/* dont fragment flags */
-#define IP_MF 0x2000		/* more fragments flag */
-#define IP_OFFMASK 0x1fff	/* mask for fragmented bits */
-	u_char ip_ttl;			/* time to live */
-	u_char ip_p;			/* protocol */
-	u_short ip_sum;			/* checksum */
-	struct in_addr ip_src, ip_dst; 	/* source and dest addresses */
-};
-
-#define SIZE_ETHERNET 14
-#define SIZE_WLAN 30
-
-/*extern pcap_t *handle;*/
-/*extern char streamip[16];*/
-
-void callback_detect_stream(u_char *arg, const struct pcap_pkthdr *pkthdr, const u_char *packet); 
-
-#endif
->>>>>>> origin
--- traffic_debug.h
+++ traffic_debug.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef TRAFFIC_DEBUG_H
 #define TRAFFIC DEBUG_H
 
@@ -29,36 +28,3 @@ void print_devices();
 int main(int argc, char **argv);
 
 #endif
-=======
-#ifndef TRAFFIC_DEBUG_H
-#define TRAFFIC DEBUG_H
-
-#include <pcap.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <errno.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netinet/if_ether.h>
-#include <netinet/tcp.h>
-#include <netinet/ip.h>
-#include <string.h>
-#include <sys/time.h>
-#include <unistd.h>
-#include "detect_stream.h"
-#include "handle_init.h"
-#include "callback_detect_stream.h"
-#include "callback_stream_analyze.h"
-#include "callback_stream_log.h"
-#include "global.h"
-
-pcap_t *handle;
-char streamip[16];
-
-void usage(char *name, int code);
-void print_devices();
-int main(int argc, char **argv);
-
-#endif
->>>>>>> origin
