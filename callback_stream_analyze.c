42f0985e3c9b26ab9f9a1b8c4aed72461c9d5241
Matthew Chin <Matthew Chin>
2016-10-23 23:37:31 +0100

    Capture Statistics
    added functionality to capture statistics from opened device
    
    added option for capture direction

--- callback_stream_analyze.c
+++ callback_stream_analyze.c
@@ -1,6 +1,6 @@
 #include "callback_stream_analyze.h"
 
-void callback_stream_analyze(u_char *arg, const struct pcap_pkthdr *pkthdr, const u_char *packet){
-	static int count = 1;
-	printf("Packet number [%d]\ttime: %ld %ld\n", count++, (long int)(pkthdr->ts.tv_sec), (long int)(pkthdr->ts.tv_usec));
+void callback_stream_analyze(u_char *arg, const struct pcap_pkthdr *pkthdr, const u_char *packet) {
+    static int count = 1;
+    printf("Packet number [%d]\ttime: %ld %ld\n", count++, (long int) (pkthdr->ts.tv_sec), (long int) (pkthdr->ts.tv_usec));
 }
