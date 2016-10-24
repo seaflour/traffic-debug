dae1e4e3ec9df95e4af19659bbf9722d17133597
tom <3tomdoelp@gmail.com>
2016-10-24 10:33:00 -0400

    another merge

--- callback_stream_log.c
+++ callback_stream_log.c
@@ -1,6 +1,5 @@
 #include "callback_stream_log.h"
 
-<<<<<<< HEAD
 /**
  * creates a dump handle so that we can write packet data to the
  * specified file
@@ -8,10 +7,6 @@
  * @param pkthdr
  * @param packet
  */
-void callback_stream_log(u_char *file, const struct pcap_pkthdr *pkthdr, const u_char *packet) {
-    pcap_dump(file, pkthdr, packet);
-=======
 void callback_stream_log(u_char *arg, const struct pcap_pkthdr *pkthdr, const u_char *packet){
   pcap_dump(arg, pkthdr, packet);
->>>>>>> analyze-stream
 }
