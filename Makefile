ce44080f79d4d4fffe489c44acb4a6990ffd8845
tom <3tomdoelp@gmail.com>
2016-10-30 19:28:40 -0400

    Add TCP seq

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ CFLAGS = -Wall -Werror -g -std=gnu99
 LDFLAGS = -lpcap
 
 # List of sources
-SOURCES = traffic_debug.c handle_init.c detect_stream.c callback_detect_stream.c callback_stream_analyze.c callback_stream_log.c
+SOURCES = traffic_debug.c handle_init.c detect_stream.c callback_detect_stream.c callback_stream_analyze.c time_analysis.c callback_stream_log.c
 OBJECTS = $(SOURCES:.c=.o)
 
 # Executable target
@@ -16,13 +16,16 @@ all: $(SOURCES) $(EXECUTABLE)
 $(EXECUTABLE): $(OBJECTS)
 	$(CC) $(CFLAGS) $(OBJECTS) -o $@ $(LDFLAGS)
 
-traffic_debug.o: traffic_debug.c traffic_debug.h callback_stream_log.h callback_stream_analyze.h callback_detect_stream.h detect_stream.h handle_init.h
+traffic_debug.o: traffic_debug.c traffic_debug.h callback_stream_log.h callback_stream_analyze.h time_analysis.h callback_detect_stream.h detect_stream.h handle_init.h
 	$(CC) $(CFLAGS) -c $<
 
 callback_stream_log.o: callback_stream_log.c callback_stream_log.h
 	$(CC) $(CFLAGS) -c $<
 
-callback_stream_analyze.o: callback_stream_analyze.c callback_stream_analyze.h
+callback_stream_analyze.o: callback_stream_analyze.c callback_stream_analyze.h time_analysis.h
+	$(CC) $(CFLAGS) -c $<
+
+time_analysis.o: time_analysis.c time_analysis.h
 	$(CC) $(CFLAGS) -c $<
 
 callback_detect_stream.o: callback_detect_stream.c callback_detect_stream.h
--- callback_stream_analyze.c
+++ callback_stream_analyze.c
@@ -1,15 +1,37 @@
 #include "callback_stream_analyze.h"
 
 void callback_stream_analyze(u_char *arg, const struct pcap_pkthdr *pkthdr, const u_char *packet) {
-    static int count = 1;
-    printf("Packet number [%d]\ttime: %ld %ld\n", count++, (long int) (pkthdr->ts.tv_sec), (long int) (pkthdr->ts.tv_usec));
-	
-	time_analysis((long int) (pkthdr->ts.tv_sec), (long int) (pkthdr->ts.tv_usec), (int) (pkthdr->len));
 
-	/* tcp stuff here */
+	static int count = 1;
+	/*	static int prevlen = 0;*/
+	/*	static u_char *prev = NULL;*/
+	struct tcp_header *tcp_pack;
+	static struct tcp_header *tcp_prev = NULL;
+	int hdr_size = SIZE_IP;
 
-	//output packet
-    for (int i = 0; i < (pkthdr->caplen + 1); i++) {
-        printf("Packet: %.2x\n", packet[i - 1]);
+	if (*arg == (u_char) 'f' && tcp_prev != NULL) {
+		free(tcp_prev);
+		tcp_prev = NULL;
+	} else {
+		printf("Packet number [%d]\n", count++);
+
+		time_analysis((long int) (pkthdr->ts.tv_sec), (long int) (pkthdr->ts.tv_usec), (int) (pkthdr->len));
+
+		/* add length of link layer header to the IP header */
+		if (*arg == (u_char) 'e') {
+			hdr_size += SIZE_ETHERNET;
+		} else if (*arg == (u_char) 'w') {
+			hdr_size += SIZE_WLAN;
+		}
+		tcp_pack = (struct tcp_header*)(packet + hdr_size);
+
+		printf("seq: %u\tack: %u\n", ntohl(tcp_pack->seq), ntohl(tcp_pack->ack));
+
+		if (tcp_prev == NULL)
+			tcp_prev = malloc(sizeof(struct tcp_header));
+		else
+			printf("Prev\nseq: %u\tack %u\n\n", ntohl(tcp_prev->seq), ntohl(tcp_prev->ack));
+
+		memcpy(tcp_prev, tcp_pack, sizeof(struct tcp_header));
 	}
 }
