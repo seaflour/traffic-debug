5991ba95ca2b0556fc0afea3af2515f18b8dab43
tom <3tomdoelp@gmail.com>
2016-10-30 19:48:29 -0400

    Add suspected retransmission count

--- callback_stream_analyze.c
+++ callback_stream_analyze.c
@@ -3,11 +3,11 @@
 void callback_stream_analyze(u_char *arg, const struct pcap_pkthdr *pkthdr, const u_char *packet) {
 
 	static int count = 1;
-	/*	static int prevlen = 0;*/
-	/*	static u_char *prev = NULL;*/
+	static int retrans_min = 0, retrans_max = 0;
 	struct tcp_header *tcp_pack;
 	static struct tcp_header *tcp_prev = NULL;
 	int hdr_size = SIZE_IP;
+	unsigned int sequence, prevseq;
 
 	if (*arg == (u_char) 'f' && tcp_prev != NULL) {
 		free(tcp_prev);
@@ -17,20 +17,34 @@ void callback_stream_analyze(u_char *arg, const struct pcap_pkthdr *pkthdr, cons
 
 		time_analysis((long int) (pkthdr->ts.tv_sec), (long int) (pkthdr->ts.tv_usec), (int) (pkthdr->len));
 
-		/* add length of link layer header to the IP header */
-		if (*arg == (u_char) 'e') {
-			hdr_size += SIZE_ETHERNET;
-		} else if (*arg == (u_char) 'w') {
-			hdr_size += SIZE_WLAN;
-		}
-		tcp_pack = (struct tcp_header*)(packet + hdr_size);
-
-		printf("seq: %u\tack: %u\n", ntohl(tcp_pack->seq), ntohl(tcp_pack->ack));
-
-		if (tcp_prev == NULL)
+		if (tcp_prev == NULL) {
 			tcp_prev = malloc(sizeof(struct tcp_header));
-		else
-			printf("Prev\nseq: %u\tack %u\n\n", ntohl(tcp_prev->seq), ntohl(tcp_prev->ack));
+		} else {
+			/* add length of link layer header to the IP header */
+			if (*arg == (u_char) 'e') {
+				hdr_size += SIZE_ETHERNET;
+			} else if (*arg == (u_char) 'w') {
+				hdr_size += SIZE_WLAN;
+			}
+			tcp_pack = (struct tcp_header*)(packet + hdr_size);
+
+			sequence = ntohl(tcp_pack->seq);
+			prevseq = ntohl(tcp_prev->seq);
+
+			if (sequence < prevseq) {
+				retrans_min++;
+				retrans_max++;
+				printf("* * * suspect retransmission * * *\n");
+			}
+			if (sequence == prevseq) {
+				retrans_max++;
+				printf("* possible retransmission *\n");
+			}
+
+			printf("seq: %u\tack: %u\n", sequence, prevseq);
+			printf("seq: %u\tack: %u (prev)\n", ntohl(tcp_prev->seq), ntohl(tcp_prev->ack));
+			printf("retransmissions: %d - %d\n\n", retrans_min, retrans_max);
+		}
 
 		memcpy(tcp_prev, tcp_pack, sizeof(struct tcp_header));
 	}
