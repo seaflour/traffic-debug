19dd4b902b76248a2afcae31bdc6b8775facc60e
tom <3tomdoelp@gmail.com>
2016-11-02 13:27:13 -0400

    add time files

---
+++ time_analysis.c
@@ -0,0 +1,5 @@
+#include "time_analysis.h"
+
+
+void time_analysis(long int sec, long int usec, int length){
+}
---
+++ time_analysis.h
@@ -0,0 +1,6 @@
+#ifndef TIME_ANALYSIS_H
+#define TIME_ANALYSIS_H
+
+void time_analysis(long int sec, long int usec, int length);
+
+#endif
