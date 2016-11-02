f721aa67aab537766185d1346248144ea0897340
mjc714 <chin.matthew1212@gmail.com>
2016-11-02 13:06:27 -0400

    Add time analysis files

---
+++ time_analysis.h
@@ -0,0 +1,22 @@
+#ifndef TIME_ANALYSIS_H
+#define TIME_ANALYSIS_H
+
+#include <errno.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+long currBps;
+long avgBps;
+long ts;
+int totalPktCount;
+int pps;
+
+extern const time_t startTime;
+
+void time_analysis(long sec, long usec, int len, int caplen);
+void print_analysis(int tpc);
+
+#endif
