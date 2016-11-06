e9e451733ac458993b56fe590e4b336017c5604d
tom <3tomdoelp@gmail.com>
2016-11-05 20:04:29 -0400

    Count TCP errors and report consecutives

--- callback_stream_analyze.c
+++ callback_stream_analyze.c
@@ -3,6 +3,7 @@
 void callback_stream_analyze(u_char *arg, const struct pcap_pkthdr *pkthdr, const u_char *packet) {
 
 	static int count = 1;
+	static int bad_count = 0, good_count = 0;
 	static int err_min = 0, err_max = 0;
 	struct tcp_header *tcp_pack;
 	static struct tcp_header *tcp_prev = NULL;
@@ -14,9 +15,13 @@ void callback_stream_analyze(u_char *arg, const struct pcap_pkthdr *pkthdr, cons
 		free(tcp_prev);
 		tcp_prev = NULL;
 	} else {
-		printf("Packet number [%d]", count++);
 
-		time_analysis((long int) (pkthdr->ts.tv_sec), (long int) (pkthdr->ts.tv_usec), (int) (pkthdr->len));
+		time_analysis(
+				(long int) (pkthdr->ts.tv_sec), 
+				(long int) (pkthdr->ts.tv_usec), 
+				(int) (pkthdr->len), 
+				(int) (pkthdr->caplen));
+
 
 		if (tcp_prev == NULL) {
 			tcp_prev = malloc(sizeof(struct tcp_header));
@@ -33,18 +38,28 @@ void callback_stream_analyze(u_char *arg, const struct pcap_pkthdr *pkthdr, cons
 			prevseq = ntohl(tcp_prev->seq);
 
 			if (sequence < prevseq) {
-				err_min++;
-				err_max++;
-				printf(" * * *");
-			}
-			if (sequence == prevseq) {
-				err_max++;
-				printf(" *");
+				/* SEQ is lower... retransmission likely */	
+				good_count = 0;
+				bad_count++;
+			} else if (sequence == prevseq) {
+				/* SEQ is unchanged... retransmission possible*/
+				good_count = 0;
+				bad_count++;
+			} else {
+				/* normal SEQ */
+				good_count++;
+				/* reset bad counter if we've seen 3 good packets in a row */
+				if (good_count > 3)
+					bad_count = 0;
 			}
 
-			printf("\nseq: %u\tack: %u\n", sequence,  ntohl(tcp_pack->ack));
-			printf("seq: %u\tack: %u (prev)\n", ntohl(tcp_prev->seq), ntohl(tcp_prev->ack));
-			printf("errors: %d - %d\n\n", err_min, err_max);
+			if (bad_count > 3) {
+				printf("Packet number [%d] stream error likely!\n", count++);
+			}
+
+/*			printf("seq: %u\tack: %u (prev)\n", ntohl(tcp_prev->seq), ntohl(tcp_prev->ack));*/
+/*			printf("\nseq: %u\tack: %u\n", sequence,  ntohl(tcp_pack->ack));*/
+/*			printf("errors: %d - %d\n\n", err_min, err_max);*/
 		}
 
 		memcpy(tcp_prev, tcp_pack, sizeof(struct tcp_header));
